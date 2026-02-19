d3184731b52ffa3e6f4891141581e9a0f13eabbc
DieGon7771 <bruttodiego6@gmail.com>
2026-02-19 16:55:40 +0100

    Update MainActivity.kt

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -240,6 +240,7 @@ class MainActivity : AppCompatActivity(), ColorPickerDialogListener, BiometricCa
          * This is a very bad solution but I was unable to find a better one.
          **/
         var nextSearchQuery: String? = null
+        var pendingRepoUrl: String? = null
 
         /**
          * Fires every time a new batch of plugins have been loaded, no guarantee about how often this is run and on which thread

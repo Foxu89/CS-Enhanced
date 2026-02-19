4a5d4d4d187de66b55188d933e976d40d6906850
DieGon7771 <bruttodiego6@gmail.com>
2026-02-19 22:24:50 +0100

    Update ActorAdaptor.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/result/ActorAdaptor.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/result/ActorAdaptor.kt
@@ -83,7 +83,7 @@ class ActorAdaptor(
                 itemView.setOnLongClickListener {
                     if (isLayout(PHONE)) {
         // Get TMDB ID from the actor
-                        val actorId = item.actor.id
+                        val actorId = item.id
                         val actorName = item.actor.name
                         val actorImage = item.actor.image
         
